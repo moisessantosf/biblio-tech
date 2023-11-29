cd32e016e54f3565d68f2f1b092ef676bc6cfb19
Moisés Santos <moisessantosf@gmail.com>
2023-11-29 00:28:49 -0300

    Criação estrutura do projeto.

---
+++ src/Biblio.WebApi/Program.cs
@@ -0,0 +1,50 @@
+using Biblio.Infrastructure.Data.Context;
+using Biblio.Infrastructure.IoC;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options
+        .UseNpgsql(builder.Configuration["DbContextSettings:ConnectionString"])
+);
+
+DependencyContainer.RegisterServices(builder.Services);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("corspolicy", build =>
+    {
+        build.WithOrigins("http://localhost")
+             .AllowAnyMethod()
+             .AllowAnyHeader()
+             .AllowCredentials();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("corspolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
