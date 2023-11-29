26c7761bcbcc33c5c7ab0cad4ba633bae3ed2e1d
Moisés Santos <moisessantosf@gmail.com>
2023-11-29 00:33:08 -0300

    Inclusão suporte ao docker e docker-compose. Inclusão do projeto controlador de Inversão de Controle (injeção de dependência).

---
+++ src/Biblio.IoC/DependencyContainer.cs
@@ -0,0 +1,31 @@
+﻿using Biblio.Application.Interfaces;
+using Biblio.Application.Services;
+using Biblio.Domain.Interfaces;
+using Biblio.Infrastructure.Data.Repositories;
+using Biblio.Infrastructure.Messaging.Interfaces;
+using Biblio.Infrastructure.Messaging.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Biblio.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<IBookService, BookService>();
+            services.AddScoped<IBookRepositoryAsync, BookRepositoryAsync>();
+
+            services.AddScoped<ICustomerService, CustomerService>();
+            services.AddScoped<ICustomerRepositoryAsync, CustomerRepositoryAsync>();
+
+            services.AddScoped<ILoanService, LoanService>();
+            services.AddScoped<ILoanRepositoryAsync, LoanRepositoryAsync>();
+
+            services.AddScoped<IStockService, StockService>();
+            services.AddScoped<IStockRepositoryAsync, StockRepositoryAsync>();
+
+            services.AddSingleton<IMessagingService, MockMessagingService>();
+
+        }
+    }
+}
